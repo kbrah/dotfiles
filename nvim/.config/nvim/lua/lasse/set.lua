69f16ed1483c317936b79cb44242bcf925880d64
Lasse Linkola <lasse.linkola@enersoft.fi>
2022-11-04 16:44:42 +0200

    stuff

--- nvim/.config/nvim/lua/lasse/set.lua
+++ nvim/.config/nvim/lua/lasse/set.lua
@@ -12,12 +12,16 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
+vim.opt.cursorline = true
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>st", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {})
 
 local mark = require("harpoon.mark")
 local harpoonUi = require("harpoon.ui")
